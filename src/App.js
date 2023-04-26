17748bc5c5b57b888d8ad9b4ddea83e508d80bb1
Joao Augusto <67336838+joaoaugusto05@users.noreply.github.com>
2023-04-26 14:44:19 -0300

    basic files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {NavigationBar} from './components/NavigationBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+      <NavigationBar/>
+    </div>
+  );
+}
+
+export default App;

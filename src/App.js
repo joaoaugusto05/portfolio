5aaa42552ce0c1ae4d3538e1aced7330dd6267d5
JoaoAugusto05 <joaoaugusto_fb@usp.br>
2023-04-26 21:34:58 -0300

    started with navbar and banner

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import {NavigationBar} from './components/NavigationBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Banner } from './components/Banner';
 function App() {
   return (
     <div className="App">
       <NavigationBar/>
+      <Banner/>
     </div>
   );
 }
